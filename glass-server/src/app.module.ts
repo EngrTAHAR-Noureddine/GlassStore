2622a45fca6e5bebbcec548ac7332d74e91faa11
TAHAR Noureddine <engrtaharnoureddine@bitbucket.org>
2025-11-07 22:53:21 +0100

    feat: add new modules and configure TypeORM with PostgreSQL
    - Added AddressesModule, OrdersModule, PaymentsModule, ProductsModule, SellersModule, ShippingModule, UsersModule, and VariantsModule.
    - Integrated TypeORM for PostgreSQL database connection with dynamic configuration using environment variables.
    - Introduced ConfigModule for environment variable management and validation using Joi.
    - Implemented a LoggerMiddleware for logging HTTP requests.
    - Updated AppService to return a new greeting message.
    - Added app configuration file for better environment management.
    - Included necessary dependencies in package.json for TypeORM, PostgreSQL, and Joi.

--- glass-server/src/app.module.ts
+++ glass-server/src/app.module.ts
@@ -1,9 +1,54 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { SellersModule } from './sellers/sellers.module';
+import { ProductsModule } from './products/products.module';
+import { VariantsModule } from './variants/variants.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ShippingModule } from './shipping/shipping.module';
+import { AddressesModule } from './addresses/addresses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './common/config/app.config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    // To set Type orm to connect to the database PG  
+    TypeOrmModule.forRootAsync(
+      {
+        useFactory: () => ({
+          type: 'postgres',
+          host: process.env.DATABASE_HOST,
+          port: +(process.env.DATABASE_PORT || 5432),
+          username:  process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          autoLoadEntities: true,
+          synchronize: true,
+        })
+    }),
+    ConfigModule.forRoot({
+    load: [appConfig],
+    validationSchema: Joi.object({
+      DATABASE_HOST: Joi.string().required(),
+      DATABASE_PORT: Joi.number().default(5432).required(),
+      DATABASE_USER: Joi.string().required(),
+      DATABASE_PASSWORD: Joi.string().required(),
+      DATABASE_NAME: Joi.string().required(),
+    }),
+  }),
+    UsersModule, 
+    SellersModule, 
+    ProductsModule, 
+    VariantsModule, 
+    OrdersModule, 
+    PaymentsModule, 
+    ShippingModule, 
+    AddressesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ glass-server/src/common/config/app.config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    enviroment: process.env.NODE_ENV || 'development',
+    database: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : 5432,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        name: process.env.DATABASE_NAME,
+    },
+});
